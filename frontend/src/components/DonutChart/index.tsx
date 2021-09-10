6a7c2455f62541d35aaecba95a14be334fcf6f85
Fabioaugustmp <fabioaugustmp16@gmail.com>
2021-09-10 17:01:43 -0300

    BarChart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,5 @@
 
 import axios from 'axios';
-import { type } from 'os';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
@@ -24,7 +23,7 @@ const DonutChart = () => {
 
                 setChartData({ labels: myLabels, series: mySeries });
             });
-    }, [])
+    }, []);
 
     //const mockData = {
     //  series: [477138, 499928, 444867, 220426, 473088],
